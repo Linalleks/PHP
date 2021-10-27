6ae45b741cf3292a4528b8bce77aa0df6b3f4500
Linalleks <a_alleks@mail.ru>
2021-10-28 00:47:11 +0700

    completed lesson3

--- lesson3.php
+++ lesson3.php
@@ -5,7 +5,7 @@
 $arr = [33, 42, -7, -81, 11, -2, 21, 0, 5, 67];
 
 $index = (int) readline("Индекс = ");
-//php lesson3.php
+
 
 if (is_integer($index)) {
   if (isset($arr[$index]) !== false) {
@@ -30,3 +30,39 @@ if (is_integer($index)) {
 } else {
   echo "Введенный символ не может быть индексом\n";
 }
+
+// Задание 2
+
+$days = [
+  'monday' => 'понедельник',
+  'tuesday' => 'вторник',
+  'wednesday' => 'среда',
+  'thursday' => 'четверг',
+  'friday' => 'пятница',
+  'saturday' => 'суббота',
+  'sunday' => 'воскресенье',
+];
+
+$days_ru = array_values($days);
+$days_en = array_keys($days);
+
+echo "Дни недели на русском:\n" . implode(', ', $days_ru);
+echo "\nДни недели на английском:\n" . implode(', ', $days_en) . "\n";
+
+
+// Задание 3
+
+$text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel tempus erat. In sollicitudin nisl nisi, in cursus erat pulvinar et. In congue eleifend accumsan. Nam dictum, nibh a justo iaculis, at hendrerit dui condimentum. Nulla et malesuada elit. Etiam eu dolor et nulla lobortis lacinia malesuada quis lacus. Aliquam nec nibh porta, vehicula justo id, sodales eros. Nulla facilisi. Nulla quis dui volutpat, mattis massa ut, interdum nisl.';
+
+if (count(explode(" ", trim($text))) > 50) {
+  echo "Превышен лимит количества слов в тексте.\n";
+}
+
+$arr_text = explode(". ", trim($text));
+
+echo "\nКоличество предложений в тексте: " . count($arr_text) . "\n";
+
+$clause = implode("-", explode(" ", str_replace([".", ","], "", trim(lcfirst($arr_text[4])))));
+
+
+echo "\nСсылка из 5го предложения:\nhttp://site.ru/news/$clause\n";
